188d7fafd081b5259c5c6df19e4017d83ca85b9f
Michael Aerni <hello@michaelaerni.ch>
2022-03-11 17:33:20 -0800

    Update test bootstrap

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,34 +2,42 @@
 
 namespace Aerni\Zipper\Tests;
 
-use Aerni\Zipper\ServiceProvider as ZipperServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Statamic\Extend\Manifest;
-use Statamic\Providers\StatamicServiceProvider;
 use Statamic\Statamic;
 
 abstract class TestCase extends OrchestraTestCase
 {
-    /**
-     * Load package service provider
-     *
-     * @param \Illuminate\Foundation\Application $app
-     * @return array
-     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $uses = array_flip(class_uses_recursive(static::class));
+
+        if (isset($uses[PreventSavingStacheItemsToDisk::class])) {
+            $this->preventSavingStacheItemsToDisk();
+        }
+    }
+
+    public function tearDown(): void
+    {
+        $uses = array_flip(class_uses_recursive(static::class));
+
+        if (isset($uses[PreventSavingStacheItemsToDisk::class])) {
+            $this->deleteFakeStacheDirectory();
+        }
+
+        parent::tearDown();
+    }
+
     protected function getPackageProviders($app): array
     {
         return [
-            ZipperServiceProvider::class,
-            StatamicServiceProvider::class,
+            \Aerni\Zipper\ServiceProvider::class,
+            \Statamic\Providers\StatamicServiceProvider::class,
         ];
     }
 
-    /**
-     * Load package aliases
-     *
-     * @param \Illuminate\Foundation\Application $app
-     * @return array
-     */
     protected function getPackageAliases($app): array
     {
         return [
@@ -37,12 +45,6 @@ abstract class TestCase extends OrchestraTestCase
         ];
     }
 
-    /**
-     * Load Environment
-     *
-     * @param \Illuminate\Foundation\Application $app
-     * @return void
-     */
     protected function getEnvironmentSetUp($app): void
     {
         parent::getEnvironmentSetUp($app);
@@ -50,30 +52,47 @@ abstract class TestCase extends OrchestraTestCase
         $app->make(Manifest::class)->manifest = [
             'aerni/zipper' => [
                 'id' => 'aerni/zipper',
-                'namespace' => 'Aerni\\Zipper\\',
+                'namespace' => 'Aerni\\Zipper',
             ],
         ];
     }
 
-    /**
-     * Resolve the application configuration and set the Statamic configuration
-     *
-     * @param \Illuminate\Foundation\Application $app
-     * @return void
-     */
     protected function resolveApplicationConfiguration($app): void
     {
         parent::resolveApplicationConfiguration($app);
 
         $configs = [
-            'assets', 'cp', 'forms', 'routes', 'sites',
-            'stache', 'static_caching', 'system', 'users',
+            'assets',
+            'cp',
+            'forms',
+            'routes',
+            'sites',
+            'stache',
+            'static_caching',
+            'system',
+            'users',
         ];
 
         foreach ($configs as $config) {
             $app['config']->set("statamic.$config", require(__DIR__ . "/../vendor/statamic/cms/config/{$config}.php"));
         }
 
+        // Setting the user repository to the default flat file system
+        $app['config']->set('statamic.users.repository', 'file');
+
+        // Set the content paths for our stache stores
+        $app['config']->set('statamic.stache.stores.taxonomies.directory', __DIR__ . '/__fixtures__/content/taxonomies');
+        $app['config']->set('statamic.stache.stores.terms.directory', __DIR__ . '/__fixtures__/content/taxonomies');
+        $app['config']->set('statamic.stache.stores.collections.directory', __DIR__ . '/__fixtures__/content/collections');
+        $app['config']->set('statamic.stache.stores.entries.directory', __DIR__ . '/__fixtures__/content/collections');
+        $app['config']->set('statamic.stache.stores.navigation.directory', __DIR__ . '/__fixtures__/content/navigation');
+        $app['config']->set('statamic.stache.stores.collection-trees.directory', __DIR__ . '/__fixtures__/content/trees/collections');
+        $app['config']->set('statamic.stache.stores.nav-trees.directory', __DIR__ . '/__fixtures__/content/trees/navigation');
+        $app['config']->set('statamic.stache.stores.globals.directory', __DIR__ . '/__fixtures__/content/globals');
+        $app['config']->set('statamic.stache.stores.asset-containers.directory', __DIR__ . '/__fixtures__/content/assets');
+        $app['config']->set('statamic.stache.stores.users.directory', __DIR__ . '/__fixtures__/users');
+
+        // Define the addon config for our tests
         $app['config']->set('zipper', require(__DIR__.'/../config/zipper.php'));
     }
 }
