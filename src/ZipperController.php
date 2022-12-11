5b15b0a9e251f5236166df675a65ae4ffc8ec5a5
Michael Aerni <hello@michaelaerni.ch>
2022-12-11 13:59:04 -0800

    Add return types

--- src/ZipperController.php
+++ src/ZipperController.php
@@ -3,12 +3,14 @@
 namespace Aerni\Zipper;
 
 use Illuminate\Http\Request;
+use STS\ZipStream\ZipStream;
 use Illuminate\Support\Facades\Crypt;
 use Statamic\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ZipperController extends Controller
 {
-    public function create(string $cipher, Request $request)
+    public function create(string $cipher, Request $request): ZipStream|StreamedResponse
     {
         if (! $request->hasValidSignature()) {
             abort(403);
