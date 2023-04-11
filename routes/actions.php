a59873061e36522927cc5434f98f920e6b7a5dd3
Michael Aerni <hello@michaelaerni.ch>
2023-04-11 10:01:42 -0400

    Simplify route

--- routes/actions.php
+++ routes/actions.php
@@ -4,6 +4,6 @@ use Aerni\Zipper\Http\Controllers\ZipperController;
 use Aerni\Zipper\Http\Middleware\VerifyRequest;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/create/{id}', [ZipperController::class, 'create'])
+Route::get('{id}', [ZipperController::class, 'create'])
     ->middleware(VerifyRequest::class)
     ->name('zipper.create');
